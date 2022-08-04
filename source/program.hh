97f6d46e1b35d974a8fe1d6bc132af1fb2d38882
Andrey Proskurin <andreyproskurin@protonmail.com>
2022-08-04 03:05:13 +0200

    Cache uniform locations

--- source/program.hh
+++ source/program.hh
@@ -3,10 +3,13 @@
 #include <glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <unordered_map>
+#include <string_view>
 #include "util.hh"
 
 class Program {
 	GLuint handle {0};
+	mutable std::unordered_map<std::string_view, GLint> cache;
 public:
 	Program(char const *vertexShaderPath, char const *fragmentShaderPath)
 	{
@@ -66,12 +69,18 @@ public:
 private:
 	GLint getUniformLocation(char const *name) const
 	{
-		// TODO: Cache locations?
-		GLint loc = glGetUniformLocation(handle, name);
-		if (loc < 0) {
-			std::cout << "Uniform variable does not exist: " << name << '\n';
+		// Note that, at least on my hardware, this is actually slower than simply
+		// calling glGetUniformLocation. I only do this to keep track of which
+		// warnings have already been printed.
+		std::string_view sv = name;
+		if (cache.count(sv) == 0) {
+			GLint loc = glGetUniformLocation(handle, name);
+			if (loc < 0) {
+				std::cout << "Uniform variable does not exist: " << name << '\n';
+			}
+			cache[sv] = loc;
 		}
-		return loc;
+		return cache[sv];
 	}
 
 	static GLuint compileShader(GLenum type, char const *path)
